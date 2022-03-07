40656c816cfccd4ac1c68adb03dc406d56458c6a
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-07 14:09:45 -0600

    return lastName of the Customer in Order

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -18,7 +18,7 @@ const getOrders = async (req, res, next) => {
   try {
     // populate the customer and product fields of the order model
     const orders = await Order.find()
-      .populate('customer', 'name') // In the first level we only add the name of the model - get only the name of the customer
+      .populate('customer', ['name', 'lastName']) // In the first level we only add the name and lastName of the model - get only the name and lastName of the customer
       .populate('order.product', 'name') // If it is greater than one level, it is required to indicate nesting separated by dots - get the name of the product
 
     res.status(200).json(orders)
