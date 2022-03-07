cc2b00bbbb68088f2d31d3eafb838ff8e10f0829
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-07 15:30:25 -0600

    Endpoint and controller to signup

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,29 @@
-const signup = (req, res) => {
-  res.send('signup')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const signup = async (req, res) => {
+  const { name, email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).send('User already exists')
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    await User.create({
+      name,
+      email,
+      password: hashPassword
+    })
+
+    return res.status(201).send({ message: 'User created successfully' })
+  } catch (error) {
+    res.status(500).send('Server error')
+  }
 }
 
 const login = (req, res) => {
